eec2c89fbe52bf547cc251d3683147bb31580c43
Ryan Payne <ryan6payne@gmail.com>
2023-04-30 19:56:46 +0100

    Removing snow

--- src/App.js
+++ src/App.js
@@ -5,38 +5,6 @@ function App() {
   return (
     <div className="application">
       <main>
-        <div className="snowflakes" aria-hidden="true">
-          <div className="snowflake">
-          ❅
-          </div>
-          <div className="snowflake">
-          ❅
-          </div>
-          <div className="snowflake">
-          ❆
-          </div>
-          <div className="snowflake">
-          ❄
-          </div>
-          <div className="snowflake">
-          ❅
-          </div>
-          <div className="snowflake">
-          ❆
-          </div>
-          <div className="snowflake">
-          ❄
-          </div>
-          <div className="snowflake">
-          ❅
-          </div>
-          <div className="snowflake">
-          ❆
-          </div>
-          <div className="snowflake">
-          ❄
-          </div>
-        </div>
         <Center/>
       </main>
     </div>
