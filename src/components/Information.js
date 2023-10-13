59754893cc7ee6a0238a0cb06f0f4cab02f1e702
Ryan <ryan6payne@gmail.com>
2023-10-13 12:14:13 +0100

    Removing the test

--- src/components/Information.js
+++ src/components/Information.js
@@ -15,7 +15,7 @@ function Information() {
                     3rd place: £40
                 </p>
                 <h6 className="information-text-weeks-remaining">
-                    Ends Monday 16th October (2 rounds remain TEST)
+                    Ends Monday 16th October (2 rounds remain)
                 </h6>
             </div>
         </div>
