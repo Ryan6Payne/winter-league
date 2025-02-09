cb56d91cb6a00bede51f78bf5be0b684e0f31041
Ryan Payne <ryan6payne@gmail.com>
2025-02-09 13:05:23 +0000

    updating information

--- src/components/Information.js
+++ src/components/Information.js
@@ -6,16 +6,16 @@ function Information() {
             </div>
             <div>
             <p className="information-text">
-                    1st place: £120
+                    1st place: £105
                 </p>
                 <p className="information-text">
-                    2nd place: £80
+                    2nd place: £65
                 </p>
                 <p className="information-text">
                     3rd place: £40
                 </p>
                 <p className="information-text">
-                    The league ends in 10 rounds
+                    The league ends 5th April at Belton Woods
                 </p>
             </div>
         </div>
